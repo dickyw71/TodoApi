1de02a6597ad494868eadaa4cca534d345644107
Richard Wheatley <dickyw71@users.noreply.github.com>
2019-07-31 12:48:48 +0100

    Added POST and PUT methods to controller and .gitignore updated.

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -46,5 +46,30 @@ namespace TodoApi.Controllers
 
             return todoItem;
         }
+
+        // POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+        }
+        
+        // PUT: api/Todo/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
